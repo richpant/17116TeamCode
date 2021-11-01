66f71a3b45838551e3ffda42f0c50b0dc3bc8ff4
richpant <47926579+richpant@users.noreply.github.com>
2021-11-01 15:42:28 -0500

    commented out servo and lift motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MechaDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MechaDrive.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -13,21 +14,21 @@ public class MechaDrive extends OpMode {
     private DcMotor rightRear;
     private DcMotor leftFront;
     private DcMotor leftRear;
-    private Servo flipper;
-    private DcMotor liftMotor;
+  // private Servo flipper;
+    /*private DcMotor liftMotor;
     public static final double MID_SERVO       =  0.5 ;
     public static final double LIFT_UP_POWER    =  0.45 ;
     public static final double LIFT_DOWN_POWER  = -0.45 ;
     double          clawOffset  = 0.0 ;                  // Servo mid position
-    final double    CLAW_SPEED  = 0.02 ;                 // sets rate to move servo
+    final double    CLAW_SPEED  = 0.02 ;                 // sets rate to move servo*/
     @Override
     public void init(){
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         leftRear = hardwareMap.get(DcMotor.class, "leftRear");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         rightRear = hardwareMap.get(DcMotor.class, "rightRear");
-        flipper = hardwareMap.get(Servo.class,"flipper");
-        liftMotor = hardwareMap.get(DcMotor.class,"liftMotor");
+       // flipper = hardwareMap.get(Servo.class,"flipper");
+        //liftMotor = hardwareMap.get(DcMotor.class,"liftMotor");
        // rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
        // rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
         int pos = 0;
@@ -54,6 +55,8 @@ public class MechaDrive extends OpMode {
             rightRear.setPower(0);
             leftRear.setPower(0);
             leftFront.setPower(0);
+        }
+
         }
 
     }
